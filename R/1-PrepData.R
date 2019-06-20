53328d9824d65a6e33ff72f80641bafdc1c04885
Julie Turner <julwturner@gmail.com>
2019-06-20 16:32:17 -0500

    start more formally prepping data

---
+++ R/1-PrepData.R
@@ -0,0 +1,24 @@
+### Data ====
+# Julie Turner
+# Started: June 20 2019
+
+
+### Packages ----
+# remotes::install_github('bbolker/broom.mixed')
+libs <- c('data.table', 'spatsoc')
+lapply(libs, require, character.only = TRUE)
+
+### Input data ----
+raw <- 'data/raw-data/'
+derived <- 'data/derived-data/'
+
+### rarified data ----
+dat <- fread(paste0(raw, 'RMNPwolf_rarified.csv'))
+dat$datetime <- paste(dat$gmtDate, dat$gmtTime)
+dat$datetime <- as.POSIXct(dat$datetime, tz = 'UTC', "%Y-%m-%d %H:%M:%S")
+group_times(dat, datetime = 'datetime', threshold = '20 minutes')
+
+
+
+dat_nn <- edge_nn(dat, id = 'WolfID', coords = c('X', 'Y'),
+                 timegroup = 'timegroup', splitBy = 'PackID')
