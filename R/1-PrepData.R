e6c1bb55fb0aba3dfad4c2554994da5796d79075
Julie Turner <julwturner@gmail.com>
2019-06-20 18:16:30 -0500

    comments to remind me how to proceed

--- R/1-PrepData.R
+++ R/1-PrepData.R
@@ -18,7 +18,8 @@ dat$datetime <- paste(dat$gmtDate, dat$gmtTime)
 dat$datetime <- as.POSIXct(dat$datetime, tz = 'UTC', "%Y-%m-%d %H:%M:%S")
 group_times(dat, datetime = 'datetime', threshold = '20 minutes')
 
-
+### create steps first, then do nn, but only compare to original nn for each random step, 
+### not whoever may be the new closest  
 
 dat_nn <- edge_nn(dat, id = 'WolfID', coords = c('X', 'Y'),
                  timegroup = 'timegroup', splitBy = 'PackID')
