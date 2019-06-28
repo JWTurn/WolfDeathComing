c394d5e931cca23af966648eaf530f0afdc730f6
Julie Turner <julwturner@gmail.com>
2019-06-28 16:38:05 -0230

    The mess that I got to work or W06 nn dist

--- R/1-PrepData.R
+++ R/1-PrepData.R
@@ -5,6 +5,7 @@
 
 ### Packages ----
 # remotes::install_github('bbolker/broom.mixed')
+# remotes::install_github('ropensci/spatsoc')
 libs <- c('data.table', 'spatsoc', 'dplyr', 'amt', 'lubridate', 'raster')
 lapply(libs, require, character.only = TRUE)
 
@@ -16,6 +17,9 @@ derived <- 'data/derived-data/'
 dat <- fread(paste0(raw, 'RMNPwolf_rarified.csv'))
 dat$datetime <- paste(dat$gmtDate, dat$gmtTime)
 dat$datetime <- as.POSIXct(dat$datetime, tz = 'UTC', "%Y-%m-%d %H:%M:%S")
+
+dat.meta <- fread(paste0(raw, 'wolf_metadata.csv'))
+dat.meta$end_date<- as.POSIXct(dat.meta$end_date, tz = 'UTC', "%Y-%m-%d")
 
 #### determining nearest neighbor (nn) at orginal point ####
 dat.grptimes <- group_times(DT=dat, datetime = "datetime", threshold = "2 hours")
@@ -165,29 +169,74 @@ ssf1 <- stps %>% random_steps(n = 9) %>%
         cos_ta = cos(ta_)) 
 ssf1[,'ttd1'] <- as.duration(ssf1$t1_ %--% as.POSIXct("2016-06-26 23:59:59", tz = 'UTC', "%Y-%m-%d %H:%M:%S"))/ddays(1) 
 ssf1[,'ttd2'] <- as.duration(ssf1$t2_ %--% as.POSIXct("2016-06-26 23:59:59", tz = 'UTC', "%Y-%m-%d %H:%M:%S"))/ddays(1) 
-DT.nn <- dplyr::select(DT.prep, t, nn)
+DT.nn <- dplyr::select(DT.prep, t, id, nn)
 ssf <- merge(ssf1, DT.nn, by.x = 't1_', by.y = 't', all.x = T)
 colnames(ssf)[colnames(ssf)=="nn"] <- "nn1"
-ssf <- merge(ssf, DT.nn, by.x = 't2_', by.y = 't', all.x = T)
+ssf <- merge(ssf, DT.nn, by.x = c('t2_', 'id'), by.y = c('t', 'id'), all.x = T)
 colnames(ssf)[colnames(ssf)=="nn"] <- "nn2"
 
 
+
 ### calculate all distances ###
-remotes::install_github('ropensci/spatsoc')
-library(spatsoc)
-DT.grp <- select(dat.grptimes, WolfID, PackID, X, Y, datetime, timegroup)
-DT.randpts <- merge(ssf, DT.grp, by.y = c('datetime', 'X', 'Y'), by.x = c('t2_', 'x2_', 'y2_'), all.x =T)
+
+dat.grp <- select(dat.grptimes, WolfID, PackID, X, Y, datetime, timegroup)
+dat.randpts <- merge(ssf, dat.grp, by.y = c('datetime', 'WolfID'), 
+                     by.x = c('t2_', 'id'), all =T)
+dat.randpts$x2_ <- ifelse(!is.na(dat.randpts$x2_), dat.randpts$x2_, dat.randpts$X)
+dat.randpts$y2_ <- ifelse(!is.na(dat.randpts$y2_), dat.randpts$x2_, dat.randpts$Y)
+
+DT.randpts <-setDT(dat.randpts)
+DT.randpts <- DT.randpts[t2_ >= as.POSIXct("2016-06-26 23:59:59", tz = 'UTC', "%Y-%m-%d %H:%M:%S") %m-% months(1)]
+DT.randpts <- DT.randpts[, step_id_rank:= 1:.N, by=.(step_id_)]
+DT.randpts$step_id_rank <- ifelse(!is.na(DT.randpts$step_id_), DT.randpts$step_id_rank, 1)
+
+DT.randpts[,'id.step'] <- paste(DT.randpts$id, DT.randpts$step_id_rank, sep = '.')
+DT.randpts[,'id.step.t'] <- paste(DT.randpts$id.step, DT.randpts$timegroup, sep = '.')
+
+
 edist2 <- edge_dist(
   DT = DT.randpts,
-  threshold = 100000, # I don't know how to pick this
-  id = 'WolfID',
+  threshold = 10000000, # I don't know how to pick this
+  id = 'id.step.t',
   coords = c('x2_', 'y2_'),
-  timegroup = 't2_',
+  timegroup = 'timegroup',
   returnDist = TRUE,
   splitBy = 'PackID'
 )
 
-### didn't work, can't read it for some reason
+
+
+edist2.ww <- edist2[PackID =='WW']
+edist2.ww.sep <- tidyr::separate(edist2.ww, col = ID1, into = c('id1', 'step1', 't1'))
+edist2.ww.sep <- tidyr::separate(edist2.ww.sep, col = ID2, into = c('id2', 'step2', 't2'))
+edist2.ww.sep[,'step'] <- as.integer(edist2.ww.sep$step1)
+
+edist2.ww.sep<- edist2.ww.sep[,.(timegroup, step, id1, id2, distance)]
+
+
+#edist2.ww.sep <- edist2.ww.sep[id1 != id2]
+DT.grp[,'round.t'] <- as.POSIXct(round(DT.grp$datetime, units = 'hours'), tz = 'UTC', "%Y-%m-%d %H:%M:%S")
+
+
+DT.grp2 <- setDT(DT.grp)[,.(round.t), by = .(timegroup)] 
+DT.grp2 <- unique(DT.grp2)
+
+ssf.soc <- setDT(ssf)[, step_id_rank:= 1:.N, by=.(step_id_)]
+ssf.soc<- merge(ssf.soc, DT.grp2, by.x = 't2_', by.y = 'round.t', all.x = T)
+colnames(ssf.soc)[colnames(ssf.soc)=="timegroup"] <- "timegroup2"
+ssf.soc<- merge(ssf.soc, DT.grp2, by.x = 't1_', by.y = 'round.t', all.x = T)
+colnames(ssf.soc)[colnames(ssf.soc)=="timegroup"] <- "timegroup1"
+
+
+ssf.soc <- merge(ssf.soc, edist2.ww.sep, by.x = c('id','nn2', 'step_id_rank', 'timegroup2'),
+                 by.y = c('id1','id2', 'step', 'timegroup'), all.x = T)
+
+DT.temp <- DT.randpts[,.(id, t2_, step_id_rank), timegroup]
+DT.nn2 <- merge(DT.nn, DT.temp, by.x = c('id','t'), by.y = c('id', 't2_'), all.x = T)
+
+DT.nn.dist <- merge(DT.nn2, edist2.ww.sep, by.x = c('timegroup', 'step_id_rank', 'id', 'nn'), 
+                    by.y = c('timegroup', 'step1', 'id1', 'id2'), all.y = T)
+
 
 #######
 group_times(dat, datetime = 'datetime', threshold = '20 minutes')
