7070e4987aaab2384f4b53f718c4d5addb973253
Julie Turner <julwturner@gmail.com>
2019-07-16 14:40:26 -0230

    switching to proportion of land types and adjusting for controls

--- R/1-Analyses.R
+++ R/1-Analyses.R
@@ -24,51 +24,11 @@ dat <- dat[, stepjum := forcats::fct_shuffle(as.character(step_id_)), by = .(id)
 
 dat[,.(nstep = uniqueN(as.character(step_id_)), nstepjum = uniqueN(stepjum)), by = .(id)]
 
+dat.meta <- fread(paste0(raw, 'wolf_metadata.csv'))
 
-#### adding prop landcover, but move to data prep ####
-# Generate spatial points object from UTM coordinates
-locs <- SpatialPoints(data.frame(dat$x1_, dat$y1_))
-locs_end <- SpatialPoints(data.frame(dat$x2_, dat$y2_))
 
-# Read in the landcover map with different habitat types
-lc <-  raster(paste0(raw, 'RMNPlandcover.tif'), )
-cland <- fread(paste0(raw, 'rcl_cowu.csv'))
-lc <- raster::reclassify(lc, cland)
-# To get rid of NAs, I make them a new category (10), and then add it to the legend
-lc[is.na(lc)] <- 5
 
-closed <- lc == 1
-names(closed) <- "closed"
-open <- lc == 2
-names(open) <- "open"
-wet <- lc == 3
-names(wet) <- "wet"
-
-## This creates an object which can be used to make a layer of specified diameter
-# The d value is what determines the buffer size if you want to change it.
-## If you're doing multiple landcover classes, you only need to run this line once, as long as each of the habitat variables has the same resolution
-# (e.g., the "Wetland" here could be any cover type)
-
-Buff100 <- focalWeight(closed, d=100, type = 'circle')
-
-## This generates a new raster where each cell corresponds to the mean wetland within a 100m buffer.
-# Since it's all 1s and 0s, this is the same as the proportion of wetland surrounding the focal variable
-wetBuff100 <- focal(wet, Buff100, na.rm = TRUE, pad = TRUE, padValue = 0)
-closedBuff100 <- focal(closed, Buff100, na.rm = TRUE, pad = TRUE, padValue = 0)
-openBuff100 <- focal(open, Buff100, na.rm = TRUE, pad = TRUE, padValue = 0)
-
-## You can then use the "extract" function as you would on any habitat layer and add it to your data:
-dat[,'propwet_start'] <- extract(wetBuff100, locs)
-dat[,'propclosed_start'] <- extract(closedBuff100, locs)
-dat[,'propopen_start'] <- extract(openBuff100, locs)
-
-dat[,'propwet_end'] <- extract(wetBuff100, locs_end)
-dat[,'propclosed_end'] <- extract(closedBuff100, locs_end)
-dat[,'propopen_end'] <- extract(openBuff100, locs_end)
-
-#saveRDS(dat, 'data/derived-data/ssfAllCov.Rds')
-
-# dat[,'ua'] <- ifelse(dat$case_ == T, 'used', 'avail')
+dat[,'ua'] <- ifelse(dat$case_ == T, 'used', 'avail')
 # dat[,'wet'] <- ifelse(dat$land_end == 'wet', 'wet', 'not')
 # dat.wet<-dat[land_end=='wet', .(id, ttd1, ua, propwet_end)]
 # dat.wet[, .(id, .N), by= c('ua', 'id')]
@@ -88,9 +48,11 @@ ggplot(dat.wet.used, aes(x=factor(dtd),y=(wetpd), group=(id)))+
 lattice::xyplot(wetpd ~ dtd, data = dat.wet.used, groups = id, type ='l')
 lattice::xyplot(propwet_end ~ dtd, data = dat.wet.used, groups = id, type ='l')
 
-dat.meta <- fread(paste0(raw, 'wolf_metadata.csv'))
 
-dat.wet[,cor(propwet_end, ttd1), by=.(id)]
+dat.wet[ua=='used',cor(propwet_end, ttd1), by=.(id)]
+
+
+
 
 #### fit models ####
 
@@ -111,17 +73,23 @@ dat.wet[,cor(propwet_end, ttd1), by=.(id)]
 # out[,'term']<-c('coef','hr','se','z','p')
 
 #### CORE ####
+model <- clogit(case_ ~ log_sl:ToD_start + land_end + 
+                  #sl:closed + sl:open + sl:wet + 
+                  strata(stepjum), data = dat[id=='W06'])
+sum.mod <- summary(model)
+sum.mod$conf.int
 
-Core <- function(y, sl, ToD, closed, open, wet, strata1) {
+
+Core <- function(y, sl, ToD, wet, open, strata1) {
     # Make the model
-    model <- clogit(y ~ sl:ToD + closed + open + wet + 
-                      #sl:closed + sl:open + sl:wet + 
+    model <- clogit(y ~ sl:ToD + wet + open +
+                      sl:wet + sl:open +
                       strata(strata1))
     sum.model <- summary(model)$coefficients
     # Transpose the coef of the model and cast as data.table
     term <- c('coef','hr','se','z','p')
     coefOut <- data.table(t(sum.model))
-
+    
     # Return combined columns
     # print(summary(model))
     print(AIC(model))
@@ -130,9 +98,10 @@ Core <- function(y, sl, ToD, closed, open, wet, strata1) {
 
 #run iSSA model by ID
 # Core Model
-coreOUT<- dat[, Core(case_, log_sl, ToD_start, propclosed_end, propopen_end, propwet_end, stepjum), by = id]
+coreOUT<- dat[, Core(case_, log_sl, ToD_start, propwet_end, propopen_end, stepjum), by = id]
 m.core <- merge(coreOUT, dat.meta, by.x = 'id', by.y = 'WolfID', all.x = T)
 m.core.p<-m.core[term=='coef' | term=='p']
+
 
 
 m.full <- data.all %>% amt::fit_issf(case_ ~ log_sl:ToD_start + land_end + log_sl:land_end + 
@@ -144,9 +113,10 @@ m.full <- data.all %>% amt::fit_issf(case_ ~ log_sl:ToD_start + land_end + log_s
 
 #### MOVEMENT ####
 
-Move <- function(y, sl, ToD, land, ttd, ta, strata1) {
+Move <- function(y, sl, ToD, wet, open, ttd, ta, strata1) {
   # Make the model
-  model <- clogit(y ~ sl:ToD + land + sl:land + 
+  model <- clogit(y ~ sl:ToD + wet + open +
+                    sl:wet + sl:open +
                     log(ttd+1):sl + log(ttd+1):ta + strata(strata1))
   sum.model <- summary(model)$coefficients
   # Transpose the coef of the model and cast as data.table
@@ -159,18 +129,19 @@ Move <- function(y, sl, ToD, land, ttd, ta, strata1) {
   return(data.table(term, coefOut, AIC=AIC(model)))
 }
 
-moveOUT <- dat[, Move(case_, log_sl, ToD_start, land_end, ttd1, cos_ta, stepjum), by = id]
+moveOUT <- dat[, Move(case_, log_sl, ToD_start, propwet_end, propopen_end, ttd1, cos_ta, stepjum), by = id]
 m.move <- merge(moveOUT, dat.meta, by.x = 'id', by.y = 'WolfID', all.x = T)
-m.move.p<-m.move[term=='coef' | term=='p', .(id, `sl:ttd`, `ttd:ta`, COD)]
+m.move.p<-m.move[term=='coef' | term=='p', .(id, term, `sl:log(ttd + 1)`, `log(ttd + 1):ta`, COD)]
 
 
 
 #### HABITAT ####
 
-Habitat <- function(y, sl, ToD, land, ttd, parkdist, parkYN, strata1) {
+Habitat <- function(y, sl, ToD, wet, open, ttd, parkdist, parkYN, strata1) {
   # Make the model
-  model <- clogit(y ~ sl:ToD + land + sl:land + 
-                    log(ttd+1):land + log(ttd+1):parkdist:parkYN + strata(strata1))
+  model <- clogit(y ~ sl:ToD + wet + open +
+                    sl:wet + sl:open +
+                    log(ttd+1):wet + log(ttd+1):open + log(ttd+1):parkdist:parkYN + strata(strata1))
   sum.model <- summary(model)$coefficients
   # Transpose the coef of the model and cast as data.table
   term <- c('coef','hr','se','z','p')
@@ -182,9 +153,9 @@ Habitat <- function(y, sl, ToD, land, ttd, parkdist, parkYN, strata1) {
   return(data.table(term, coefOut, AIC=AIC(model)))
 }
 
-habOUT <- dat[, Habitat(case_, log_sl, ToD_start, land_end, ttd1, lnparkdist_end, parkYN_end, stepjum), by = id]
+habOUT <- dat[, Habitat(case_, log_sl, ToD_start, propwet_end, propopen_end, ttd1, lnparkdist_end, parkYN_end, stepjum), by = id]
 m.habitat <- merge(habOUT, dat.meta, by.x = 'id', by.y = 'WolfID', all.x = T)
-m.habitat.p <- m.habitat[term=='coef'|term=='p', .(id, term, `landclosed:log(ttd + 1)`, `landopen:log(ttd + 1)`, `landwet:log(ttd + 1)`, 
+m.habitat.p <- m.habitat[term=='coef'|term=='p', .(id, term, `wet:log(ttd + 1)`, `open:log(ttd + 1)`, 
                        `log(ttd + 1):parkdist:parkYNpark`, `log(ttd + 1):parkdist:parkYNout-park`, COD)]
 
 Habitat.land <- function(y, sl, ToD, land, ttd, strata1) {
@@ -236,9 +207,10 @@ m.social <- data.all %>% amt::fit_issf(case_ ~ log_sl:ToD_start + land_end + log
                                         log(ttd1):log(distance2) + 
                                         strata(step_id_))
 
-Social <- function(y, sl, ToD, land, ttd, nndist, packYN, packdist, strata1) {
+Social <- function(y, sl, ToD, wet, open, ttd, nndist, packYN, packdist, strata1) {
   # Make the model
-  model <- clogit(y ~ sl:ToD + land + sl:land + 
+  model <- clogit(y ~ sl:ToD + wet + open +
+                    sl:wet + sl:open +
                     log(ttd+1):log(nndist+1) + log(ttd+1):log(packdist+1):packYN + strata(strata1))
   sum.model <- summary(model)$coefficients
   # Transpose the coef of the model and cast as data.table
@@ -251,15 +223,30 @@ Social <- function(y, sl, ToD, land, ttd, nndist, packYN, packdist, strata1) {
   return(data.table(term, coefOut, AIC=AIC(model)))
 }
 
-dat[id !='W13' & id != 'W11' & id != 'W19' & id != 'W15',unique(id)]
+dat[,unique(id)] 
+dat[id !='W05' & id != 'W03' & id != 'W07' & id != 'W19' & id != 'W15' & id != 'W14',unique(id)]
 # W13, W11, W27,  not converge
 
-socOUT <- dat[id !='W13' & id != 'W11' & id != 'W19' & id != 'W15', Social(case_, log_sl, ToD_start, land_end, ttd1, distance2, packYN_end, packDist_end, stepjum), by = id]
+socOUT <- dat[id !='W05' & id != 'W03' & id != 'W07' & id != 'W19' & id != 'W15' & id != 'W14', Social(case_, log_sl, ToD_start, propwet_end, propopen_end, ttd1, distance2, packYN_end, packDist_end, as.integer(stepjum)), by = id]
 m.social <- merge(socOUT, dat.meta, by.x = 'id', by.y = 'WolfID', all.x = T)
-m.social.p <- m.social[term=='coef' |term=='p', .(id, term,`ttd:log(nndist + 1)`, `ttd:log(packdist + 1):packYNpack`, `ttd:log(packdist + 1):packYNout-pack`, COD)]
+m.social.p <- m.social[term=='coef' |term=='p', .(id, term, `log(ttd + 1):log(nndist + 1)`, `log(ttd + 1):log(packdist + 1):packYNpack`, `log(ttd + 1):log(packdist + 1):packYNout-pack`, COD)]
 
-
+dat.focal<- dat.meta[status=='dead',.(WolfID, death=as.Date(end_date))]
+dat.focal <- dat.focal[WolfID %chin% unique(dat$id)] 
+range(dat.focal$death)
+plot(dat.focal$death)
 
 bbmle::AICtab(m.core$model, m.move$model, m.habitat.parkdist$model, m.social$model)
+
+
+
+range(dat$step_id_)
+range(as.integer(dat$stepjum))
+
+dat.steps <- dat[,.(id, step_id_, stepjum, stepjumnum = as.integer(stepjum))]
+
+
+
+
 
 
