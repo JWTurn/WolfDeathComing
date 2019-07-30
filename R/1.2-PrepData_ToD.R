03d17f55da9b7e816307b81544b507dbacc022b2
Julie Turner <julwturner@gmail.com>
2019-07-30 12:55:14 -0230

    updating covariate output for new land types
    coniferous, mixed

--- R/1.2-PrepData_ToD.R
+++ R/1.2-PrepData_ToD.R
@@ -45,10 +45,10 @@ fullmst[,'ToD_start'] <- ifelse((fullmst$SunriseDate + hours(1)) < fullmst$t1mst
                                 ifelse(fullmst$TwiStart > fullmst$t1mst | fullmst$t1mst > fullmst$TwiEnd, 'night', 'twilight'))
 
 # get rid of now unneeded columns
-ssf.wolf <- fullmst[,.(burst_, step_id_, case_, x1_, y1_, x2_, y2_, t1_, t2_, dt_, sl_, log_sl, ta_, cos_ta, ToD_start, tod_amt_start = tod_start_, 
+ssf.wolf <- fullmst[,.(burst_, step_id_, case_, x1_, y1_, x2_, y2_, t1_, t2_, dt_, sl_, log_sl, ta_, cos_ta, ToD_start,
                            parkYN_start, parkYN_end, roadDist_start, roadDist_end, parkDist_end, lnparkdist_start, lnparkdist_end, parkDistadj_end, 
-                       land_start, land_end, propwet_start, propwet_end, propopen_start, propopen_end, propclosed_start, propclosed_end, wet_end, open_end, closed_end,
-                           id, nn1, nn2, distance1, distance2, timegroup1, timegroup2, packYN_start, packYN_end, packDist_start, packDist_end, packDistadj_end,
+                       land_start, land_end, propwet_end, propopen_end, propconif_end, propmixed_end, wet_end, open_end, conif_end, mixed_end,
+                       id, nn1, nn2, distance1, distance2, timegroup1, timegroup2, packYN_start, packYN_end, packDist_start, packDist_end, packDistadj_end,
                            ttd1, ttd2)]
 
 saveRDS(ssf.wolf, 'data/derived-data/ssfAllCov.Rds')
