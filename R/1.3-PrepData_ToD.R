1fbd69393c67382981db559c4070f798d3986b10
Julie Turner <julwturner@gmail.com>
2019-08-05 14:05:23 -0400

    adjusting for GHA26

--- R/1.3-PrepData_ToD.R
+++ R/1.3-PrepData_ToD.R
@@ -26,11 +26,13 @@ day <- readRDS('data/derived-data/sunsetsunriseRMNP_2016-2017.Rds')
 daymst <- day[,.(Date, TwiStartDate, SunriseDate, SunsetDate, TwiEndDate)]
 
 # Read in SSF
-ssf.soc <- readRDS("data/derived-data/ssfAll.Rds")
+ssf.soc <- readRDS("data/derived-data/ssfAll_GHA26.Rds")
 
+# not needed for GHA26
 ssf.soc[, 't1mst'] <- ssf.soc$t1_ - hours(6)
 ssf.soc[, 't2mst'] <- ssf.soc$t2_ - hours(6)
-ssf.soc[,'date1mst'] <- as.POSIXct(format(ssf.soc$t1mst, "%Y-%m-%d"), tz = 'UTC', "%Y-%m-%d")
+#ssf.soc[,'date1mst'] <- as.POSIXct(format(ssf.soc$t1mst, "%Y-%m-%d"), tz = 'UTC', "%Y-%m-%d")
+ssf.soc[,'date1mst'] <- as.POSIXct(format(ssf.soc$t1_, "%Y-%m-%d"), tz = 'UTC', "%Y-%m-%d")
 
 
 
@@ -45,10 +47,16 @@ fullmst[,'ToD_start'] <- ifelse((fullmst$SunriseDate + hours(1)) < fullmst$t1mst
                                 ifelse(fullmst$TwiStart > fullmst$t1mst | fullmst$t1mst > fullmst$TwiEnd, 'night', 'twilight'))
 
 # get rid of now unneeded columns
+# ssf.wolf <- fullmst[,.(burst_, step_id_, case_, x1_, y1_, x2_, y2_, t1_, t2_, dt_, sl_, log_sl, ta_, cos_ta, ToD_start,
+#                            parkYN_start, parkYN_end, roadDist_start, roadDist_end, parkDist_end, lnparkdist_start, lnparkdist_end, parkDistadj_end, 
+#                        land_start, land_end, propwet_end, propopen_end, propconif_end, propmixed_end, wet_end, open_end, conif_end, mixed_end,
+#                        id, nn1, nn2, distance1, distance2, timegroup1, timegroup2, packYN_start, packYN_end, packDist_start, packDist_end, packDistadj_end,
+#                            ttd1, ttd2)]
+
 ssf.wolf <- fullmst[,.(burst_, step_id_, case_, x1_, y1_, x2_, y2_, t1_, t2_, dt_, sl_, log_sl, ta_, cos_ta, ToD_start,
-                           parkYN_start, parkYN_end, roadDist_start, roadDist_end, parkDist_end, lnparkdist_start, lnparkdist_end, parkDistadj_end, 
+                       roadDist_start, roadDist_end, 
                        land_start, land_end, propwet_end, propopen_end, propconif_end, propmixed_end, wet_end, open_end, conif_end, mixed_end,
                        id, nn1, nn2, distance1, distance2, timegroup1, timegroup2, packYN_start, packYN_end, packDist_start, packDist_end, packDistadj_end,
-                           ttd1, ttd2)]
+                       ttd1, ttd2)]
 
-saveRDS(ssf.wolf, 'data/derived-data/ssfAllCov.Rds')
+saveRDS(ssf.wolf, 'data/derived-data/ssfAllCov_GHA26.Rds')
