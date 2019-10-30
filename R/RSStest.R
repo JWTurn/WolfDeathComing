375bb4bf23baf50536fea2eb144682603972d627
Julie Turner <julwturner@gmail.com>
2019-10-30 11:54:23 -0230

    Update RSStest.R

--- R/RSStest.R
+++ R/RSStest.R
@@ -30,34 +30,43 @@ nndist.w <- readRDS('data/derived-data/socprop1moCoef.Rds')
 
 W02 <- nndist.w[wolfID=='RMNP_W02',.(betann_coef, betann_lwr, betann_upr, betaintx_coef, betaintx_lwr, betaintx_upr)]
 
+W26 <- nndist.w[wolfID=='RMNP_W26',.(betann_coef, betann_lwr, betann_upr, betaintx_coef, betaintx_lwr, betaintx_upr)]
+
 W06 <- nndist.w[wolfID=='RMNP_W06',.(betann_coef, betann_lwr, betann_upr, betaintx_coef, betaintx_lwr, betaintx_upr)]
+
+W09 <- nndist.w[wolfID=='GHA26_W09',.(betann_coef, betann_lwr, betann_upr, betaintx_coef, betaintx_lwr, betaintx_upr)]
+W27 <- nndist.w[wolfID=='RMNP_W27',.(betann_coef, betann_lwr, betann_upr, betaintx_coef, betaintx_lwr, betaintx_upr)]
+
 # Population RSS at three levels of road availability
 # delta hi should be based off avg/median nndist
-hi <- 101:1000
-delta.hi <- 100
+# hi <- 101:1000
+# delta.hi <- 100
 
-# hi <- 1001:20000
-# delta.hi <- 1000
+#### go back and change to simpler equation and log everything in it to make scales same
 
-# ttd 1mo, 2 weeks, 1 day
-# # 1day
-# hj.1 <- log(1 + 1)
-# # 2wks
-# hj.2 <- log(1 + 14)
-# # 1mo
-# hj.3 <- log(1 + 30)
+hi <- 1001:5000 # territories max ~5km probably closer 2-3km (find avg diameter of HR)
 
+delta.hi <- 1000
+
+#ttd 1mo, 2 weeks, 1 day
 # 1day
-hj.1 <- 1
+hj.1 <- log(1 + 1)
 # 2wks
-hj.2 <- 14
+hj.2 <- log(1 + 14)
 # 1mo
-hj.3 <- 30
+hj.3 <- log(1 + 30)
+
+# # 1day
+# hj.1 <- 1
+# # 2wks
+# hj.2 <- 14
+# # 1mo
+# hj.3 <- 30
 
 
-rss.nn.1 <- (log(hi/(hi-delta.hi)))^(W02$betann_coef + (W02$betaintx_coef*hj.1))
-rss.nn.2 <- (log(hi/(hi-delta.hi)))^(W02$betann_coef + (W02$betaintx_coef*hj.2))
-rss.nn.3 <- (log(hi/(hi-delta.hi)))^(W02$betann_coef + (W02$betaintx_coef*hj.3))
+rss.nn.1 <- (log(hi/(hi-delta.hi)))^(W26$betann_coef + (W26$betaintx_coef*hj.1))
+rss.nn.2 <- (log(hi/(hi-delta.hi)))^(W26$betann_coef + (W26$betaintx_coef*hj.2))
+rss.nn.3 <- (log(hi/(hi-delta.hi)))^(W26$betann_coef + (W26$betaintx_coef*hj.3))
 
 r =  ggplot() + geom_hline(yintercept = 0,colour = "black",lty = 2, size = .7)
 r = r + geom_line(aes(x=(hi-100),y=(rss.nn.1), colour = "1 day"), size = 1) 
@@ -79,7 +88,7 @@ r = r + theme(plot.title=element_text(size=20,hjust = 0.05),axis.text.x = elemen
 r = r + theme(axis.text.x = element_text(margin=margin(10,10,10,10,"pt")),
               axis.text.y = element_text(margin=margin(10,10,10,10,"pt")))+ theme(axis.ticks.length = unit(-0.25, "cm")) 
 r = r + ylab("RSS") + xlab("Distance to NN (m)")
-r = r + ylim(-0.01,1)
+r = r + ylim(-0.01,3)
 r = r +scale_colour_manual("", 
                            values = c("gray", "black", "gray33"))  
 r = r +  theme(legend.key = element_blank()) + theme(legend.position = c(.75,.9)) + theme(legend.text = element_text(size = 20))
