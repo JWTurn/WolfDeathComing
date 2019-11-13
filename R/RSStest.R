9db87818da8562d56faa59d538ff071f5934e4f7
Julie Turner <julwturner@gmail.com>
2019-11-13 10:19:37 -0330

    Update RSStest.R change to basic interaction question

--- R/RSStest.R
+++ R/RSStest.R
@@ -44,9 +44,9 @@ W27 <- nndist.w[wolfID=='RMNP_W27',.(betann_coef, betann_lwr, betann_upr, betain
 
 #### go back and change to simpler equation and log everything in it to make scales same
 
-hi <- 1001:5000 # territories max ~5km probably closer 2-3km (find avg diameter of HR)
+# hi <- 1001:5000 # territories max ~5km probably closer 2-3km (find avg diameter of HR)
 
-delta.hi <- 1000
+delta.hi <- log(1:5000)
 
 #ttd 1mo, 2 weeks, 1 day
 # 1day
@@ -63,19 +63,25 @@ hj.3 <- log(1 + 30)
 # # 1mo
 # hj.3 <- 30
 
+# ### RSS for two log-transformed interaction betas
+# rss.nn.1 <- (log(hi/(hi-delta.hi)))^(W26$betann_coef + (W26$betaintx_coef*hj.1))
+# rss.nn.2 <- (log(hi/(hi-delta.hi)))^(W26$betann_coef + (W26$betaintx_coef*hj.2))
+# rss.nn.3 <- (log(hi/(hi-delta.hi)))^(W26$betann_coef + (W26$betaintx_coef*hj.3))
 
-rss.nn.1 <- (log(hi/(hi-delta.hi)))^(W26$betann_coef + (W26$betaintx_coef*hj.1))
-rss.nn.2 <- (log(hi/(hi-delta.hi)))^(W26$betann_coef + (W26$betaintx_coef*hj.2))
-rss.nn.3 <- (log(hi/(hi-delta.hi)))^(W26$betann_coef + (W26$betaintx_coef*hj.3))
+### simpler RSS for not log-transformed betas Δhi ⋅(βi + βij ⋅hj (x1 )] 
+rss.nn.1 <- delta.hi*(W27$betann_coef + (W27$betaintx_coef*hj.1))
+rss.nn.2 <- delta.hi*(W27$betann_coef + (W27$betaintx_coef*hj.2))
+rss.nn.3 <- delta.hi*(W27$betann_coef + (W27$betaintx_coef*hj.3))
+
 
 r =  ggplot() + geom_hline(yintercept = 0,colour = "black",lty = 2, size = .7)
-r = r + geom_line(aes(x=(hi-100),y=(rss.nn.1), colour = "1 day"), size = 1) 
+r = r + geom_line(aes(x=(delta.hi),y=(rss.nn.1), colour = "1 day"), size = 1) 
 #r = r + geom_line(aes(x=(hj-250),y=log(rssroadd_lo), colour = "Day"), size = 1, lty = 3) 
 #r = r + geom_line(aes(x=(hj-250),y=log(rssroadd_hi), colour = "Day"), size = 1, lty = 3) 
-r = r + geom_line(aes(x=(hi-100),y=(rss.nn.2), colour = "14 days"), size = 1) 
+r = r + geom_line(aes(x=(delta.hi),y=(rss.nn.2), colour = "14 days"), size = 1) 
 # r = r + geom_line(aes(x=(hj-250),y=log(rssroadt_lo), colour = "Twilight"), size = 1, lty = 3) 
 # r = r + geom_line(aes(x=(hj-250),y=log(rssroadt_hi), colour = "Twilight"), size = 1, lty = 3) 
-r = r + geom_line(aes(x=(hi-100),y=(rss.nn.3), colour = "30 days"),size = 1) 
+r = r + geom_line(aes(x=(delta.hi),y=(rss.nn.3), colour = "30 days"),size = 1) 
 # r = r + geom_line(aes(x=(hj-250),y=log(rssroadn_lo), colour = "Night"),size = 1, lty = 3) 
 # r = r + geom_line(aes(x=(hj-250),y=log(rssroadn_hi), colour = "Night"),size = 1, lty = 3) 
 r = r + theme_bw()  + theme(
@@ -87,8 +93,8 @@ r = r + theme_bw()  + theme(
 r = r + theme(plot.title=element_text(size=20,hjust = 0.05),axis.text.x = element_text(size=20), axis.title = element_text(size=25),axis.text.y = element_text(size=20))
 r = r + theme(axis.text.x = element_text(margin=margin(10,10,10,10,"pt")),
               axis.text.y = element_text(margin=margin(10,10,10,10,"pt")))+ theme(axis.ticks.length = unit(-0.25, "cm")) 
-r = r + ylab("RSS") + xlab("Distance to NN (m)")
-r = r + ylim(-0.01,3)
+r = r + ylab("RSS") + xlab("log Distance to NN (m)")
+#r = r + ylim(-0.01,3)
 r = r +scale_colour_manual("", 
                            values = c("gray", "black", "gray33"))  
 r = r +  theme(legend.key = element_blank()) + theme(legend.position = c(.75,.9)) + theme(legend.text = element_text(size = 20))
